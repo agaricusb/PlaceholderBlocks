98023a962e45e504a87e36bb56a62269d68b845f
Agaricus <agaricusb@yahoo.com>
2013-03-30 15:09:03 -0700

    Remove unused texture overrides

--- src/main/java/agaricus/mods/placeholderblocks/BlockLimestone.java
+++ src/main/java/agaricus/mods/placeholderblocks/BlockLimestone.java
@@ -1,35 +1,11 @@
 package agaricus.mods.placeholderblocks;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IconRegister;
-import net.minecraft.util.Icon;
-import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
 
 public class BlockLimestone extends Block {
 
     public BlockLimestone(int id, Material material) {
         super(id, material);
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void registerIcons(IconRegister iconRegister) {
-        this.blockIcon = iconRegister.registerIcon("placeholderblocks:limestone");
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public Icon getBlockTexture(IBlockAccess world, int x, int y, int z, int blockSide) {
-        return this.blockIcon;
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public Icon getBlockTextureFromSideAndMetadata(int side, int metadata) {
-        return this.blockIcon;
     }
 }
